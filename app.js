efc82f8b9e8eb7c9b6cec262ff7d4e5908ca0c55
ashu <ashusaini17354@gmail.com>
2023-09-10 00:27:42 +0530

    cors

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const user=require('./routes/userRoutes')
+const cors=require('cors');
 
 app.use(express.json());
-
+const corsOptions = {
+    origin: 'https://frontendrepos.vercel.app', // Replace with the appropriate frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  
+  app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/v1", user);
 
